da85812270d3f4569150cdfebff7e49c95258bcc
abssailesh <abssailesh@gmail.com>
2025-02-04 00:31:18 +0530

    igniting the app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     'div',
@@ -9,8 +11,5 @@ const parent = React.createElement(
 ),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am an h1 tag"),React.createElement("h2",{},"I am an h2 tag")]
 ));
 
-
-// const heading = React.createElement('h1',{id:"Heading"},"Hello From React !");
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
